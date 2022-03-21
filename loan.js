6b27126daeeb9b965046d46fd35509bbe16baeae
JWA8390 <98052937+JWA8390@users.noreply.github.com>
2022-03-21 13:38:56 -0500

    Add files via upload

---
+++ loan.js
@@ -0,0 +1,30 @@
+var $ = function (id) 
+{
+    return document.getElementById(id);
+}
+var floatMonthlyPayment, floatTotalCost, intLoanTerm
+
+/*
+ *start
+   declare floatMonthlyPayment, intLoanTerm, floatTotalCost
+   get floatMonthlyPayment
+   get intLoanTerm
+   calculate floatTotalCost = floatMonthlyPayment * intLoanTerm
+   output floatTotalCost
+  end
+*/
+ var calculate = function()
+ {
+    floatMonthlyPayment = parseFloat($("monthly_payment").value);
+    intLoanTerm = parseInt($("loan_length").value);
+    floatTotalCost = parseFloat(floatTotalCost * intLoanTerm);
+    $("total_cost").value = floatTotalCost
+ }
+ 
+ window.onload = function () 
+{
+    $("loan_length").value = "";
+    $("calc").onclick = calculate;
+    $("monthly_payment").focus();
+    $("total_cost").value = floatTotalCost;
+}
